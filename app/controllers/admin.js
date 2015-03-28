321d0343dcf4cb537696d7c32f2adb23fdd52dcd
Alex Brinkman <alex@enlightenedpixel.com>
2015-03-28 14:19:15 -0600

    Basic form entry for all fields on the admin route.

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -3,15 +3,35 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions : {
     update : function () {
-
       var score = this.get('content');
-      // var atBat = this.get('atBat');
-      // var pitching = this.get('pitching');
-      // debugger;
-
-      // score.set('atBat', atBat);
-      // score.set('pitching', pitching);
       score.save();
+    },
+
+    valueUp : function (field) {
+      var model = this.get('content');
+      var value = model.get(field) + 1;
+      model.set(field, value);
+      model.save();
+    },
+
+    valueDown : function (field) {
+      var model = this.get('content');
+      var value = model.get(field) - 1;
+      if (value < 0){
+        value = 0;
+      }
+      model.set(field, value);
+      model.save();
+    },
+
+    toggle : function (field) {
+      var model = this.get('content');
+      var value = model.get('topBottom');
+      value = value == "top" ? "bottom" : "top";
+      model.set(field, value);
+      model.save();
     }
+
   }
+
 });
